b55fedbf149706fdb4c6f1b398231b90aaf1ca1f
nduy79 <ngocduy0997@gmail.com>
2019-07-10 19:24:06 +0700

    commit 1: first init

---
+++ src/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+var Joi = require('@hapi/joi');
+var consumer = require('./consumer');
+var producer = require('./producer');
+Joi.objectId = require('joi-objectid')(Joi);
+
+const init = async () => {
+   
+    const server = Hapi.server({
+        // host: 'localhost',
+        port: process.env.PORT || 3000
+    });
+
+
+    server.route({
+        method: 'POST',
+        path: '/orders/{id}',
+        handler:  producer.sendMessage,            
+        
+        options: {           
+            validate : producer.validatePayload
+        }
+    });
+    
+    await server.start()
+    console.log(`Server running at: ${server.info.uri}`);
+    consumer;
+}
+   
+
+ 
+
+
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
